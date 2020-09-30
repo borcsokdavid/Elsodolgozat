5e6f872afe34772de205953715b23a9cdda46a34
Börcsök Dávid <borcsok.david@szbisz.local>
2020-09-30 14:59:55 +0200

    2.Kommit

--- java.js
+++ java.js
@@ -1,38 +1,67 @@
-'use strict'
+'use strict';
 
-const diakok =[
-    
-    {nev: 'Tóth Balázs', osztály: '14szf'},
-    {nev: 'Börcsök Dávid', osztály: '14szf'},
-    {nev: 'Kucsera Kinga', osztály: '14szf'},
-    {nev: 'Pásztor Krisztián', osztály: '14szf'},
-    {nev: 'Szabó Levente', osztály: '14szf'},
-    {nev: 'Visnyei Ádám', osztály: '14szf'},
+let students = [
+    { 'név' : 'Tóth Balázs', 'osztály' : '14SZF' },
+    { 'név' : 'Börcsök Dávid', 'osztály' : '14SZF' },
+    { 'név' : 'Kiss László', 'osztály' : '14SZF' },
+    { 'név' : 'Németh Christopher', 'osztály' : '14SZF' },
+    { 'név' : 'Kucsera Kinga', 'osztály' : '14SZF' },
+    { 'név' : 'Visnyei Ádám', 'osztály' : '14SZF' },
+    { 'név' : 'Pásztor Krisztián', 'osztály' : '14SZF' },
+    { 'név' : 'Szabó Levente', 'osztály' : '14SZF' },
 ];
 
-function getPosts(){
-        
-        let output = '<table>';
-        output += '<tr><th>Osztály</th><th>Név</th></tr>';
-        
-        
-        diakok.forEach((student) => {
-           
-            output += `<tr><td>${student.osztály}</td><td>${student.nev}</td></tr>`;
-            
-        })
-        output += '</table>';
-        document.getElementById('tablazat').innerHTML = output
-    
+function osztalyListazas() {
+    let szoveg = '<table>';
+    szoveg += '<tr><th>Osztály</th><th>Név</th></tr>';
+    students.forEach((student) => {
+        szoveg += `<tr><td>${student.osztály}</td><td>${student.név}</td></tr>`;
+    });
+    szoveg +=  '</table>';
+    document.getElementById('tablazat').innerHTML = szoveg;
 }
 
-getPosts();
+osztalyListazas();
 
-
-function feltolt(){
+function feltolt() {
     let nev = document.getElementById('nev').value;
     let osztaly = document.getElementById('osztaly').value;
     console.log(nev + ' ' + osztaly);
-    diakok.push({ 'osztaly': `${osztaly}`, 'nev': `${nev}` })
-    getPosts();
+
+
+    let nevTomb = [];
+
+    students.forEach((student) => {
+        nevTomb.push(student.név);
+    });
+
+    if (nevTomb.includes(nev)){
+        window.alert("Ez a név már szerepel a listában!");
+    }
+    else{
+        students.push({ 'osztály' : `${osztaly}`, 'név' : `${nev}` });
+    }
+    /*let eldont = false;
+    students.forEach((student) => {
+        if (student.név == nev) {
+             eldont = true;
+        }
+    });
+    if (eldont) {
+        window.alert("Ez a név már szerepel a listában!");
+    }
+    else{
+        students.push({ 'osztály' : `${osztaly}`, 'név' : `${nev}` });
+    }*/
+    
+    osztalyListazas();
 }
+
+function utolsoTorol(){
+    students.pop();
+    osztalyListazas();
+}
+
+
+
+
